3a279a9f3173fc9f756142fb46a4f7d31497a835
Michael Bunch <asher12000@gmail.com>
2019-11-07 11:52:30 -0500

    Container tests.

---
+++ tests/install_tools.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 &&
+chmod +x container-structure-test-linux-amd64 &&
+mv container-structure-test-linux-amd64 container-structure-test
